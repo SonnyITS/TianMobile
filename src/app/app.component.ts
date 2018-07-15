ebb3673f5c1992d0a6d6bbe90cc59f5a4d36d60b
Yuansun Jiang <yuansun.jiang@gmail.com>
2018-07-15 02:13:54 -0400

    User input in side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -10,8 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 })
 export class MyApp {
   rootPage:any = TabsPage;
+  public Locations = [];
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private alertController: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +20,37 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+
+  openLocationAlert(){
+    let addLocationAlert = this.alertController.create({
+
+      title: "Add a Location",
+      message: "Enter Your Location",
+      inputs: [
+        {
+          type: "text",
+          name: "addLocationInput"
+        }
+      ],
+
+      buttons: [{
+          text: "Cancel"
+        },
+        {
+          text: "Add Location",
+          handler: (inputData)=>{
+          let LocationText;
+          LocationText = inputData.addLocationInput;
+          this.Locations.push(LocationText)
+        }
+        }
+      ]
+    });
+
+    addLocationAlert.present();
+
+  }
+
+
 }
