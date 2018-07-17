596336a5997db0d87777b8cf1c3ce69bb249fc11
Yuansun Jiang <yuansun.jiang@gmail.com>
2018-07-17 00:43:10 -0400

    Created Weather Provider for OpenWeatherMap API

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,17 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the WeatherProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class WeatherProvider {
+  private apiKey = '03280758d083925391d8b5a357c1906d';
+  private url;
+
 
   constructor(public http: HttpClient) {
     console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.openweathermap.org/data/2.5/weather';
+  }
+
+  getWeather(city, state){
+    return this.http.get(this.url + '?q=' + city + ',' + state + '&appid=' + this.apiKey);
   }
 
 }
